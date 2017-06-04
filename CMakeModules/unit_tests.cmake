ac757bc3d0eae7101881eb0dafdb2032b652fe9d
Lukas Hutak <xhutak01@stud.fit.vutbr.cz>
2017-06-04 14:50:50 +0200

    base2: unit tests: CMake script moved into the module directory

--- CMakeModules/unit_tests.cmake
+++ CMakeModules/unit_tests.cmake
@@ -53,8 +53,8 @@ endif()
 
 # ------------------------------------------------------------------------------
 # Register test cases
-# Param: _file   File with a test case
 function(ipx_add_test_file _file)
+# Param: _file   File with a test case
 	# Get a test name
 	get_filename_component(CASE_NAME "${_file}" NAME_WE)
 	set(CASE_NAME test_${CASE_NAME})
@@ -67,13 +67,6 @@ function(ipx_add_test_file _file)
 	add_executable(${CASE_NAME} ${_file} ${SRCS} ${HEADERS})
 	target_link_libraries(${CASE_NAME} PUBLIC libgtest)
 
-	if (ENABLE_TESTS_COVERAGE)
-		set_target_properties(${CASE_NAME}
-			PROPERTIES COMPILE_FLAGS "-g -O0 --coverage -fprofile-arcs -ftest-coverage")
-		set_target_properties(${CASE_NAME}
-			PROPERTIES LINK_FLAGS    "-g -O0 --coverage -fprofile-arcs -ftest-coverage")
-	endif()
-
 	# Add the test
 	add_test(NAME ${CASE_NAME} COMMAND "$<TARGET_FILE:${CASE_NAME}>")
 
@@ -83,15 +76,12 @@ function(ipx_add_test_file _file)
 			COMMAND ${PATH_VALGRIND} ${VALGRIND_FLAGS} "$<TARGET_FILE:${CASE_NAME}>"
 			)
 	endif()
-
-	# Remember all test cases for code coverage dependencies
-	ipx_files_add(UNIT_TESTS $<TARGET_FILE:${CASE_NAME}>)
 endfunction()
 
 
 # ------------------------------------------------------------------------------
 # Code coverage
-function(ipx_register_coverage)
+function(coverage_add_target)
 	# Find requried programs
 	find_program(PATH_GCOV    NAMES gcov)
 	find_program(PATH_LCOV    NAMES lcov)
@@ -117,8 +107,6 @@ function(ipx_register_coverage)
 	set(COVERAGE_DIR        "${CMAKE_BINARY_DIR}/code_coverage/")
 	set(COVERAGE_FILE_RAW   "${CMAKE_BINARY_DIR}/coverage_raw.info")
 	set(COVERAGE_FILE_CLEAN "${CMAKE_BINARY_DIR}/coverage_clean.info")
-	# Get the list of all binary files of test cases
-	ipx_files_get(UNIT_TESTS_TARGETS UNIT_TESTS)
 
 	# Add coverage target
 	add_custom_target(coverage
@@ -128,7 +116,6 @@ function(ipx_register_coverage)
 		COMMAND "${PATH_LCOV}" --directory . --zerocounters --quiet
 
 		# Run tests
-		#DEPENDS ${UNIT_TESTS_TARGETS}
 		COMMAND "${CMAKE_CTEST_COMMAND}" --quiet
 
 		# Capture the counters
@@ -160,9 +147,4 @@ function(ipx_register_coverage)
 
 endfunction()
 
-function(ipx_code_coverage)
-	if (ENABLE_TESTS_COVERAGE)
-		ipx_register_coverage()
-	endif()
-endfunction()
 
