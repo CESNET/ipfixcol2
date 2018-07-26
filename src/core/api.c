2ad3b35742da8c3bc1a3d4369be811f7c2e29cff
Lukas Hutak <xhutak01@stud.fit.vutbr.cz>
2018-07-26 14:47:29 +0200

    core: add API for default configuration directory

---
+++ src/core/api.c
@@ -0,0 +1,52 @@
+/**
+ * \file src/core/api.c
+ * \author Lukas Hutak <lukas.hutak@cesnet.cz>
+ * \brief Definitions for API functions (source file)
+ * \date 2018
+ */
+
+/* Copyright (C) 2018 CESNET, z.s.p.o.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of the Company nor the names of its contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * ALTERNATIVELY, provided that this notice is retained in full, this
+ * product may be distributed under the terms of the GNU General Public
+ * License (GPL) version 2 or later, in which case the provisions
+ * of the GPL apply INSTEAD OF those given above.
+ *
+ * This software is provided ``as is'', and any express or implied
+ * warranties, including, but not limited to, the implied warranties of
+ * merchantability and fitness for a particular purpose are disclaimed.
+ * In no event shall the company or contributors be liable for any
+ * direct, indirect, incidental, special, exemplary, or consequential
+ * damages (including, but not limited to, procurement of substitute
+ * goods or services; loss of use, data, or profits; or business
+ * interruption) however caused and on any theory of liability, whether
+ * in contract, strict liability, or tort (including negligence or
+ * otherwise) arising in any way out of the use of this software, even
+ * if advised of the possibility of such damage.
+ *
+ */
+
+#include <ipfixcol2/api.h>
+#include <build_config.h>
+
+/** Statically allocated string with default configuration directory */
+static const char *ipx_dir_str = IPX_DEFAULT_CONFIG_DIR;
+
+const char *
+ipx_api_cfg_dir()
+{
+    return ipx_dir_str;
+}
