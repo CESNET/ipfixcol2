495a9847d782e0479cb605187783e8593be7864d
Martin Madzia <469027@mail.muni.cz>
2019-12-17 00:30:48 +0100

    unnecessary variables removed

--- js/app.js
+++ js/app.js
@@ -21,147 +21,7 @@ const defaultConfig = {
         }
     }
 };
-const inputModulesAvailable = [
-    {
-        name: "UDP input",
-        plugin: "udp",
-        params: {
-            localPort: 4739,
-            localIPAddress: "127.0.0.1",
-            connectionTimeout: null,
-            templateLifeTime: null,
-            optionsTemplateLifeTime: null
-        }
-    },
-    {
-        name: "TCP input",
-        plugin: "tcp",
-        params: {
-            localPort: 4739,
-            localIPAddress: "127.0.0.1"
-        }
-    }
-];
-const intermediateModulesAvailable = [
-    {
-        name: "Flow anonymization",
-        plugin: "anonymization",
-        params: {
-            type: ["CryptoPAn", "Truncation"],
-            key: ""
-        }
-    }
-];
-const outputModulesAvailable = [
-    {
-        name: "JSON output",
-        plugin: "json",
-        params: {
-            tcpFlags: ["formatted", "raw"],
-            timestamp: ["formatted", "unix"],
-            protocol: ["formatted", "raw"],
-            ignoreUnknown: [true, false],
-            ignoreOptions: [true, false],
-            nonPrintableChar: [true, false],
-            outputs: {
-                server: [{ name: "", port: 0, blocking: [true, false] }],
-                send: [
-                    {
-                        name: "",
-                        ip: "",
-                        port: 0,
-                        protocol: ["tcp", "udp"],
-                        blocking: [true, false]
-                    }
-                ],
-                file: [
-                    {
-                        name: "",
-                        path: "",
-                        prefix: "",
-                        timeWindow: 300,
-                        timeAlignment: ["yes", "no"]
-                    }
-                ],
-                print: { name: "" }
-            }
-        }
-    },
-    {
-        name: "Dummy output",
-        plugin: "dummy",
-        params: {
-            delay: 0
-        }
-    },
-    {
-        name: "LNF storage",
-        plugin: "lnfstore",
-        params: {
-            storagePath: "",
-            compress: ["no", "yes"],
-            identificatorField: "",
-            dumpInterval: {
-                timeWindow: 300,
-                align: ["yes", "no"]
-            },
-            index: {
-                enable: ["no", "yes"],
-                autosize: ["yes", "no"],
-                estimatedItemCount: 100000,
-                falsePositiveProbability: 0.01
-            }
-        }
-    },
-    {
-        name: "UniRec plugin",
-        plugin: "unirec",
-        params: {
-            uniRecFormat: "",
-            trapIfcCommon: {
-                timeout: ["HALF_WAIT", "WAIT", "NO_WAIT"],
-                buffer: [true, false],
-                autoflush: 500000
-            },
-            trapIfcSpec: [
-                {
-                    unix: {
-                        name: "",
-                        maxClients: 64
-                    }
-                },
-                {
-                    tcp: {
-                        port: 0,
-                        maxClients: 64
-                    }
-                },
-                {
-                    "tcp-tls": {
-                        port: 0,
-                        maxClients: 64,
-                        keyFile: "",
-                        certFile: "",
-                        caFile: ""
-                    }
-                },
-                {
-                    file: {
-                        name: "",
-                        mode: ["write", "append"],
-                        time: 0,
-                        size: 0
-                    }
-                }
-            ]
-        }
-    }
-];
-const modulesAvailable = [
-    inputModulesAvailable,
-    intermediateModulesAvailable,
-    outputModulesAvailable
-];
+
 const x2js = new X2JS();
 
 class App extends React.Component {
@@ -181,11 +41,6 @@ class Form extends React.Component {
             modules: [[], [], []],
             overlay: null
         };
-
-        // this.state.modules[0].push(modulesAvailable[0][1]);
-        // this.state.modules[0].push(modulesAvailable[0][0]);
-        // this.state.modules[2].push(modulesAvailable[2][0]);
-        // this.state.modules[1].push(modulesAvailable[1][0]);
     }
 
     editCancel() {
@@ -292,7 +147,7 @@ class Form extends React.Component {
                         modules={this.state.modules[0]}
                         color={colors[0]}
                         name={columnNames[0]}
-                        modulesAvailable={modulesAvailable[0]}
+                        modulesAvailable={moduleSchemas[0]}
                         addModule={this.newModuleOverlay.bind(this)}
                     />
                     <FormColumn
@@ -302,7 +157,7 @@ class Form extends React.Component {
                         modules={this.state.modules[1]}
                         color={colors[1]}
                         name={columnNames[1]}
-                        modulesAvailable={modulesAvailable[1]}
+                        modulesAvailable={moduleSchemas[1]}
                         addModule={this.newModuleOverlay.bind(this)}
                     />
                     <FormColumn
@@ -312,7 +167,7 @@ class Form extends React.Component {
                         modules={this.state.modules[2]}
                         color={colors[2]}
                         name={columnNames[2]}
-                        modulesAvailable={modulesAvailable[2]}
+                        modulesAvailable={moduleSchemas[2]}
                         addModule={this.newModuleOverlay.bind(this)}
                     />
                     <Button variant="contained" color="primary">
@@ -380,7 +235,7 @@ class ModuleAvailable extends React.Component {
     };
 
     render() {
-        return <button onClick={this.handleAdd}>{this.props.module.plugin}</button>;
+        return <button onClick={this.handleAdd}>{this.props.module.properties.plugin.const}</button>;
     }
 }
 
