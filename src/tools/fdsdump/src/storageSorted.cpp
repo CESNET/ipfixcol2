a092af21eb311c0232f0cd81ec0ed7e3767e3ade
Lukas Hutak <hutak@cesnet.cz>
2022-02-10 12:43:04 +0100

    fdsdump: storageSorted: define component

---
+++ src/tools/fdsdump/src/storageSorted.cpp
@@ -0,0 +1,65 @@
+
+#include <cassert>
+#include <iterator>
+
+#include "storageSorted.hpp"
+
+StorageSorted::StorageSorted(StorageSorter sorter, size_t capacity)
+    : m_sorter{sorter}, m_records{m_sorter}, m_capacity{capacity}
+{
+}
+
+void
+StorageSorted::insert(struct Flow *flow)
+{
+    const enum Direction dir_backup = flow->dir;
+
+    if ((dir_backup & DIRECTION_FWD) != 0) {
+        flow->dir = DIRECTION_FWD;
+        insert_single_direction(flow);
+    }
+
+    if ((dir_backup & DIRECTION_REV) != 0) {
+        flow->dir = DIRECTION_REV;
+        insert_single_direction(flow);
+    }
+
+    flow->dir = dir_backup;
+}
+
+void
+StorageSorted::insert_single_direction(struct Flow *flow)
+{
+    // Exactly single direction must be specified
+    assert(flow->dir == DIRECTION_FWD || flow->dir == DIRECTION_REV);
+
+    if (m_capacity == 0 || m_records.size() < m_capacity) {
+        insert_storage_record(flow);
+        return;
+    }
+
+    const auto last_iter = std::prev(m_records.end());
+    const Flow &last_rec = last_iter->get_flow_const();
+
+    if (!m_sorter(*flow, last_rec)) {
+        // Don't insert the record as it should be placed after the last one
+        return;
+    }
+
+    m_records.erase(last_iter);
+    insert_storage_record(flow);
+}
+
+void
+StorageSorted::insert_storage_record(struct Flow *flow)
+{
+    shared_tsnapshot snapshot{
+        fds_tsnapshot_deep_copy(flow->rec.snap),
+        &fds_tsnapshot_destroy};
+
+    if (!snapshot) {
+        throw std::runtime_error("fds_tsnapshot_deep_copy() has failed");
+    }
+
+    m_records.emplace(flow->rec, flow->dir, snapshot);
+}
