298657aec8e927963d7a9a379440553f74134de5
Michal Sedlak <sedlakm@cesnet.cz>
2024-03-14 15:22:37 +0100

    UDP input: Add support for getting size of next UDP datagram in BSD compatible way

--- src/plugins/input/udp/udp.c
+++ src/plugins/input/udp/udp.c
@@ -915,20 +915,34 @@ static void
 process_socket(struct udp_data *instance, int sd)
 {
     const char *err_str;
+    ssize_t ret;
 
     // Get size of the message
     int msg_size;
+#if defined(__FreeBSD__) || defined(__OpenBSD__)
+    // ioctl(FIONREAD) on Linux returns the size of the next datagram available to read,
+    // ioctl(FIONREAD) on *BSD returns the _total_ size of _all_ datagrams available to read
+    ret = recv(sd, NULL, 0, MSG_PEEK | MSG_TRUNC);
+    if (ret == -1) {
+        ipx_strerror(errno, err_str);
+        IPX_CTX_ERROR(instance->ctx, "Unable to get size of a next datagram. recv() failed: %s",
+            err_str);
+        return;
+    }
+    msg_size = ret;
+#else
     if (ioctl(sd, FIONREAD, &msg_size) == -1) {
         ipx_strerror(errno, err_str);
         IPX_CTX_ERROR(instance->ctx, "Unable to get size of a next datagram. ioctl() failed: %s",
             err_str);
         return;
     }
+#endif
 
     if (msg_size < (int) sizeof(uint16_t) || msg_size > UINT16_MAX) {
         // Remove the malformed message from the buffer
         uint16_t mini_buffer;
-        ssize_t ret = recvfrom(sd, &mini_buffer, sizeof(mini_buffer), 0, NULL, NULL);
+        ret = recvfrom(sd, &mini_buffer, sizeof(mini_buffer), 0, NULL, NULL);
         if (ret == -1) {
             ipx_strerror(errno, err_str);
             IPX_CTX_WARNING(instance->ctx, "An error has occurred during reading a malformed "
@@ -950,7 +964,7 @@ process_socket(struct udp_data *instance, int sd)
     // Get the message
     struct sockaddr_storage addr;
     socklen_t addr_len = sizeof(addr);
-    ssize_t ret = recvfrom(sd, buffer, (size_t) msg_size, 0, (struct sockaddr *) &addr, &addr_len);
+    ret = recvfrom(sd, buffer, (size_t) msg_size, 0, (struct sockaddr *) &addr, &addr_len);
     if (ret == -1) {
         // Failed
         ipx_strerror(errno, err_str);
