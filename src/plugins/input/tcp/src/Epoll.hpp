7f500a1db54f9bab1d760c68ba0d3cda91a0d601
Jakub Antonín Štigler <jakubstig@gmail.com>
2024-03-28 15:38:25 +0100

    TCP - Craete C++ epoll wrapper

---
+++ src/plugins/input/tcp/src/Epoll.hpp
@@ -0,0 +1,58 @@
+/**
+ * \file
+ * \author Jakub Antonín Štigler <xstigl00@stud.fit.vutbr.cz>
+ * \brief C++ wrapper around epoll (header file)
+ * \date 2024
+ *
+ * Copyright: (C) 2023 CESNET, z.s.p.o.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#pragma once
+
+#include <sys/epoll.h> // epoll_event
+
+#include "UniqueFd.hpp" // UniqueFd
+
+namespace tcp_in {
+
+class Epoll {
+public:
+    /**
+     * @brief Craetes empty epoll.
+     * @throws when fails to create epoll.
+     */
+    Epoll();
+
+    /**
+     * @brief Add file descriptor to the epoll.
+     * @param fd File descriptor to add.
+     * @param [in] data data asociated with the file descriptor, when null, descriptor is set as
+     * data
+     * @throws when fails to add to the epoll.
+     */
+    void add(int fd, void *data);
+
+    /**
+     * @brief Waits for any of the file descriptors in the epoll to be active.
+     * @param[out] ev store all active descriptors here
+     * @param max_events max number of events to store in `events`
+     * @param timeout maximum time to wait in milliseconds, -1 for infinite wait
+     * @returns Number of events written to `events`, negative on error (errno is set)
+     */
+    int wait(epoll_event *events, int max_events, int timeout = -1);
+
+    /**
+     * @brief Removes file descriptor from the epoll.
+     * @param fd File descriptor to remove from the epoll.
+     * @return true on success, otherwise false
+     */
+    inline bool remove(int fd) noexcept {
+        return epoll_ctl(m_fd.get(), EPOLL_CTL_DEL, fd, nullptr) == 0;
+    }
+
+private:
+    UniqueFd m_fd;
+};
+
+} // namespace tcp_in
