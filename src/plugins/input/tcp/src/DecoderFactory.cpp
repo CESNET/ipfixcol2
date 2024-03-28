96aa8487bee64c2aee98e8907e327167de2ba7ac
Jakub Antonín Štigler <jakubstig@gmail.com>
2024-03-28 15:43:18 +0100

    TCP - Add decoder factory

---
+++ src/plugins/input/tcp/src/DecoderFactory.cpp
@@ -0,0 +1,88 @@
+/**
+ * \file
+ * \author Jakub Antonín Štigler <xstigl00@stud.fit.vutbr.cz>
+ * \brief Factory for creating decoders (source file)
+ * \date 2024
+ *
+ * Copyright: (C) 2023 CESNET, z.s.p.o.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "DecoderFactory.hpp"
+
+#include <memory>    // unique_ptr
+#include <array>     // array
+#include <cstdint>   // uint8_t, uint16_t
+#include <stdexcept> // runtime_error
+#include <cerrno>    // errno
+#include <string>    // string
+#include <cstddef>   // size_t
+
+#include <sys/socket.h> // recv, MSG_PEEK, MSG_WAITALL
+#include <netinet/in.h> // ntohl, ntohs
+
+#include <ipfixcol2.h> // ipx_strerror
+
+#include "Decoder.hpp"      // Decoder
+#include "Lz4Decoder.hpp"   // LZ4_MAGIC, Lz4Decoder
+#include "IpfixDecoder.hpp" // IPFIX_MAGIC, IpfixDecoder
+
+#include <iostream>
+
+namespace tcp_in {
+
+using namespace std;
+
+DecoderFactory::DecoderFactory() {};
+
+unique_ptr<Decoder> DecoderFactory::detect_decoder(int fd) {
+    // number of bytes neaded to detect the decoder
+    constexpr size_t MAX_MAGIC_LEN = 4;
+
+    array<uint8_t, MAX_MAGIC_LEN> buf{};
+
+    auto res = recv(fd, buf.begin(), buf.size(), MSG_PEEK | MSG_WAITALL);
+    if (res == -1) {
+        const char *err_msg;
+        ipx_strerror(errno, err_msg);
+        throw runtime_error("Failed to receive start of first message: " + string(err_msg));
+    }
+
+    constexpr const char *not_enough_data_err =
+        "Failed to read enough bytes to recognize the decoder";
+
+    // check decoders in order from shortest magic number to longest
+
+    if (res < 2) {
+        throw runtime_error(not_enough_data_err);
+    }
+
+    // IPFIX decoder
+    auto magic_u16 = ntohs(*reinterpret_cast<uint16_t *>(buf.begin()));
+    if (magic_u16 == IPFIX_MAGIC) {
+        return create_ipfix_decoder(fd);
+    }
+
+    if (res < 4) {
+        throw runtime_error(not_enough_data_err);
+    }
+
+    // LZ4 decoder
+    auto magic_u32 = ntohl(*reinterpret_cast<uint32_t *>(buf.begin()));
+    if (magic_u32 == LZ4_MAGIC) {
+        return create_lz4_decoder(fd);
+    }
+
+    throw runtime_error("Failed to recognize the decoder.");
+}
+
+unique_ptr<Decoder> DecoderFactory::create_ipfix_decoder(int fd) {
+    return unique_ptr<Decoder>(new IpfixDecoder(fd));
+}
+
+unique_ptr<Decoder> DecoderFactory::create_lz4_decoder(int fd) {
+    return unique_ptr<Decoder>(new Lz4Decoder(fd));
+}
+
+} // namespace tcp_in
+
