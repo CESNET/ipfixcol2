a0dea3589b37d5caef6c539996537c93de92042a
Michal Sedlak <sedlakm@cesnet.cz>
2024-11-14 12:25:34 +0100

    Clickhouse - add datatype

---
+++ extra_plugins/output/clickhouse/src/datatype.cpp
@@ -0,0 +1,393 @@
+/**
+ * @file
+ * @author Michal Sedlak <sedlakm@cesnet.cz>
+ * @brief Functions specific to individual data types
+ * @date 2024
+ *
+ * Copyright(c) 2024 CESNET z.s.p.o.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <type_traits>
+
+#include "datatype.h"
+#include "common.h"
+
+template <unsigned Precision> class ColumnDateTime64 : public clickhouse::ColumnDateTime64 {
+public:
+    ColumnDateTime64() : clickhouse::ColumnDateTime64(Precision) {}
+};
+
+DataType type_from_ipfix(fds_iemgr_element_type type)
+{
+    switch (type) {
+    case FDS_ET_STRING:                 return DataType::String;
+    case FDS_ET_SIGNED_8:               return DataType::Int8;
+    case FDS_ET_SIGNED_16:              return DataType::Int16;
+    case FDS_ET_SIGNED_32:              return DataType::Int32;
+    case FDS_ET_SIGNED_64:              return DataType::Int64;
+    case FDS_ET_UNSIGNED_8:             return DataType::UInt8;
+    case FDS_ET_UNSIGNED_16:            return DataType::UInt16;
+    case FDS_ET_UNSIGNED_32:            return DataType::UInt32;
+    case FDS_ET_UNSIGNED_64:            return DataType::UInt64;
+    case FDS_ET_IPV4_ADDRESS:           return DataType::IPv4;
+    case FDS_ET_IPV6_ADDRESS:           return DataType::IPv6;
+    case FDS_ET_DATE_TIME_SECONDS:      return DataType::DatetimeSecs;
+    case FDS_ET_DATE_TIME_MILLISECONDS: return DataType::DatetimeMillisecs;
+    case FDS_ET_DATE_TIME_MICROSECONDS: return DataType::DatetimeMicrosecs;
+    case FDS_ET_DATE_TIME_NANOSECONDS:  return DataType::DatetimeNanosecs;
+    default:                            throw Error("unsupported IPFIX data type {}", type);
+    }
+}
+
+static DataType unify_type(DataType a, DataType b)
+{
+    auto is_int = [](DataType t) {
+        return t == DataType::Int8
+            || t == DataType::Int16
+            || t == DataType::Int32
+            || t == DataType::Int64;
+    };
+    auto is_uint = [](DataType t) {
+        return t == DataType::UInt8
+            || t == DataType::UInt16
+            || t == DataType::UInt32
+            || t == DataType::UInt64;
+    };
+    auto is_ip = [](DataType t) {
+        return t == DataType::IPv4
+            || t == DataType::IPv6
+            || t == DataType::IP;
+    };
+    auto is_datetime = [](DataType t) {
+        return t == DataType::DatetimeSecs
+            || t == DataType::DatetimeMillisecs
+            || t == DataType::DatetimeMicrosecs
+            || t == DataType::DatetimeNanosecs;
+    };
+
+    if (a == b) {
+        return a;
+    }
+    if (is_int(a) && is_int(b)) {
+        return std::max(a, b);
+    }
+    if (is_uint(a) && is_uint(b)) {
+        return std::max(a, b);
+    }
+    if (is_datetime(a) && is_datetime(b)) {
+        return std::max(a, b);
+    }
+    if (is_ip(a) && is_ip(b)) {
+        return DataType::IP;
+    }
+
+    throw Error("cannot unify types {} and {}", a, b);
+}
+
+DataType find_common_type(const fds_iemgr_alias &alias)
+{
+    if (alias.sources_cnt == 0) {
+        throw Error("alias \"{}\" has no sources", alias.name);
+    }
+
+    DataType common_type = type_from_ipfix(alias.sources[0]->data_type);
+    for (size_t i = 1; i < alias.sources_cnt; i++) {
+        common_type = unify_type(common_type, type_from_ipfix(alias.sources[i]->data_type));
+    }
+    return common_type;
+}
+
+namespace getters {
+
+template <typename UIntType>
+static UIntType get_uint(fds_drec_field field)
+{
+    uint64_t value = 0;
+    int ret = fds_get_uint_be(field.data, field.size, &value);
+    if (ret != FDS_OK) {
+        throw Error("fds_get_uint_be() has failed: {}", ret);
+    }
+    return static_cast<UIntType>(value);
+}
+
+template <typename IntType>
+static IntType get_int(fds_drec_field field)
+{
+    int64_t value = 0;
+    int ret = fds_get_int_be(field.data, field.size, &value);
+    if (ret != FDS_OK) {
+        throw Error("fds_get_int_be() has failed: {}", ret);
+    }
+    return static_cast<IntType>(value);
+}
+
+static IP4Addr get_ipv4(fds_drec_field field)
+{
+    IP4Addr value;
+    int ret = fds_get_ip(field.data, field.size, &value);
+    if (ret != FDS_OK) {
+        throw Error("fds_get_ip() has failed: {}", ret);
+    }
+    return value;
+}
+
+static IP6Addr get_ipv6(fds_drec_field field)
+{
+    IP6Addr value;
+    int ret = fds_get_ip(field.data, field.size, &value);
+    if (ret != FDS_OK) {
+        throw Error("fds_get_ip() has failed: {}", ret);
+    }
+    return value;
+}
+
+static IP6Addr get_ip(fds_drec_field field)
+{
+    IP6Addr value;
+    int ret;
+    if (field.size == 4) {
+        static constexpr uint8_t IPV4_MAPPED_IPV6_PREFIX[]{
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0xFF, 0xFF};
+        std::memcpy(
+            reinterpret_cast<uint8_t *>(&value),
+            IPV4_MAPPED_IPV6_PREFIX,
+            sizeof(IPV4_MAPPED_IPV6_PREFIX));
+        ret = fds_get_ip(field.data, field.size, &reinterpret_cast<uint8_t *>(&value)[12]);
+    } else {
+        ret = fds_get_ip(field.data, field.size, &value);
+    }
+    if (ret != FDS_OK) {
+        throw Error("fds_get_ip() has failed: {}", ret);
+    }
+    return value;
+}
+
+static std::string get_string(fds_drec_field field)
+{
+    std::string value;
+    value.resize(field.size);
+    int ret = fds_get_string(field.data, field.size, &value[0]);
+    if (ret != FDS_OK) {
+        throw Error("fds_get_string() has failed: {}", ret);
+    }
+    return value;
+}
+
+static uint64_t get_datetime(fds_drec_field field)
+{
+    uint64_t value = 0;
+    int ret = fds_get_datetime_lp_be(field.data, field.size, field.info->def->data_type, &value);
+    if (ret != FDS_OK) {
+        throw Error("fds_get_datetime_lp_be() has failed: {}", ret);
+    }
+    value /= 1000;
+    return value;
+}
+
+template <int64_t Divisor = 1>
+static int64_t get_datetime64(fds_drec_field field)
+{
+    int64_t value = 0;
+    timespec ts;
+    int ret = fds_get_datetime_hp_be(field.data, field.size, field.info->def->data_type, &ts);
+    if (ret != FDS_OK) {
+        throw Error("fds_get_datetime_hp_be() has failed: {}", ret);
+    }
+    value = (static_cast<int64_t>(ts.tv_sec) * 1'000'000'000 + static_cast<int64_t>(ts.tv_nsec)) / Divisor;
+    return value;
+}
+}
+
+template<DataType> struct DataTypeTraits {};
+
+template<> struct DataTypeTraits<DataType::UInt8> {
+    using ColumnType = clickhouse::ColumnUInt8;
+    static constexpr std::string_view ClickhouseTypeName = "UInt8";
+    static constexpr auto Getter = &getters::get_uint<uint8_t>;
+};
+
+template<> struct DataTypeTraits<DataType::UInt16> {
+    using ColumnType = clickhouse::ColumnUInt16;
+    static constexpr std::string_view ClickhouseTypeName = "UInt16";
+    static constexpr auto Getter = &getters::get_uint<uint16_t>;
+};
+
+template<> struct DataTypeTraits<DataType::UInt32> {
+    using ColumnType = clickhouse::ColumnUInt32;
+    static constexpr std::string_view ClickhouseTypeName = "UInt32";
+    static constexpr auto Getter = &getters::get_uint<uint32_t>;
+};
+
+template<> struct DataTypeTraits<DataType::UInt64> {
+    using ColumnType = clickhouse::ColumnUInt64;
+    static constexpr std::string_view ClickhouseTypeName = "UInt64";
+    static constexpr auto Getter = &getters::get_uint<uint64_t>;
+};
+
+template<> struct DataTypeTraits<DataType::Int8> {
+    using ColumnType = clickhouse::ColumnInt8;
+    static constexpr std::string_view ClickhouseTypeName = "Int8";
+    static constexpr auto Getter = &getters::get_int<int8_t>;
+};
+
+template<> struct DataTypeTraits<DataType::Int16> {
+    using ColumnType = clickhouse::ColumnInt16;
+    static constexpr std::string_view ClickhouseTypeName = "Int16";
+    static constexpr auto Getter = &getters::get_int<int16_t>;
+};
+
+template<> struct DataTypeTraits<DataType::Int32> {
+    using ColumnType = clickhouse::ColumnInt32;
+    static constexpr std::string_view ClickhouseTypeName = "Int32";
+    static constexpr auto Getter = &getters::get_int<int32_t>;
+};
+
+template<> struct DataTypeTraits<DataType::Int64> {
+    using ColumnType = clickhouse::ColumnInt64;
+    static constexpr std::string_view ClickhouseTypeName = "Int64";
+    static constexpr auto Getter = &getters::get_int<int64_t>;
+};
+
+template<> struct DataTypeTraits<DataType::IP> {
+    using ColumnType = clickhouse::ColumnIPv6;
+    static constexpr std::string_view ClickhouseTypeName = "IPv6";
+    static constexpr auto Getter = &getters::get_ip;
+};
+
+template<> struct DataTypeTraits<DataType::IPv4> {
+    using ColumnType = clickhouse::ColumnIPv4;
+    static constexpr std::string_view ClickhouseTypeName = "IPv4";
+    static constexpr auto Getter = &getters::get_ipv4;
+};
+
+template<> struct DataTypeTraits<DataType::IPv6> {
+    using ColumnType = clickhouse::ColumnIPv6;
+    static constexpr std::string_view ClickhouseTypeName = "IPv6";
+    static constexpr auto Getter = &getters::get_ipv6;
+};
+
+template<> struct DataTypeTraits<DataType::String> {
+    using ColumnType = clickhouse::ColumnString;
+    static constexpr std::string_view ClickhouseTypeName = "String";
+    static constexpr auto Getter = &getters::get_string;
+};
+
+template<> struct DataTypeTraits<DataType::DatetimeSecs> {
+    using ColumnType = clickhouse::ColumnDateTime;
+    static constexpr std::string_view ClickhouseTypeName = "DateTime";
+    static constexpr auto Getter = &getters::get_datetime;
+};
+
+template<> struct DataTypeTraits<DataType::DatetimeMillisecs> {
+    using ColumnType = ColumnDateTime64<3>;
+    static constexpr std::string_view ClickhouseTypeName = "DateTime64(3)";
+    static constexpr auto Getter = &getters::get_datetime64<1'000'000>;
+};
+
+template<> struct DataTypeTraits<DataType::DatetimeMicrosecs> {
+    using ColumnType = ColumnDateTime64<6>;
+    static constexpr std::string_view ClickhouseTypeName = "DateTime64(6)";
+    static constexpr auto Getter = &getters::get_datetime64<1'000>;
+};
+
+template<> struct DataTypeTraits<DataType::DatetimeNanosecs> {
+    using ColumnType = ColumnDateTime64<9>;
+    static constexpr std::string_view ClickhouseTypeName = "DateTime64(9)";
+    static constexpr auto Getter = &getters::get_datetime64<1>;
+};
+
+template <typename Func>
+static void visit(DataType type, Func func)
+{
+    switch (type) {
+    case DataType::UInt8:             func(DataTypeTraits<DataType::UInt8>{});             break;
+    case DataType::UInt16:            func(DataTypeTraits<DataType::UInt16>{});            break;
+    case DataType::UInt32:            func(DataTypeTraits<DataType::UInt32>{});            break;
+    case DataType::UInt64:            func(DataTypeTraits<DataType::UInt64>{});            break;
+    case DataType::Int8:              func(DataTypeTraits<DataType::Int8>{});              break;
+    case DataType::Int16:             func(DataTypeTraits<DataType::Int16>{});             break;
+    case DataType::Int32:             func(DataTypeTraits<DataType::Int32>{});             break;
+    case DataType::Int64:             func(DataTypeTraits<DataType::Int64>{});             break;
+    case DataType::String:            func(DataTypeTraits<DataType::String>{});            break;
+    case DataType::DatetimeMillisecs: func(DataTypeTraits<DataType::DatetimeMillisecs>{}); break;
+    case DataType::DatetimeMicrosecs: func(DataTypeTraits<DataType::DatetimeMicrosecs>{}); break;
+    case DataType::DatetimeNanosecs:  func(DataTypeTraits<DataType::DatetimeNanosecs>{});  break;
+    case DataType::DatetimeSecs:      func(DataTypeTraits<DataType::DatetimeSecs>{});      break;
+    case DataType::IPv4:              func(DataTypeTraits<DataType::IPv4>{});              break;
+    case DataType::IPv6:              func(DataTypeTraits<DataType::IPv6>{});              break;
+    case DataType::IP:                func(DataTypeTraits<DataType::IP>{});                break;
+    case DataType::Invalid:           throw std::runtime_error("invalid data type");
+    }
+}
+
+std::shared_ptr<clickhouse::Column> make_column(DataType type, DataTypeNullable nullable) {
+    std::shared_ptr<clickhouse::Column> column;
+    visit(type, [&](auto traits) {
+        if (nullable == DataTypeNullable::Nullable) {
+            using ColType = clickhouse::ColumnNullableT<typename decltype(traits)::ColumnType>;
+            column = std::make_shared<ColType>();
+        } else /* (nullable == DataTypeNullable::Nonnullable) */ {
+            using ColType = typename decltype(traits)::ColumnType;
+            column = std::make_shared<ColType>();
+        }
+    });
+    return column;
+}
+
+GetterFn make_getter(DataType type) {
+    GetterFn getter;
+    visit(type, [&](auto traits) {
+        getter = [](fds_drec_field field, ValueVariant &value) { value = decltype(traits)::Getter(field); };
+    });
+    return getter;
+}
+
+ColumnWriterFn make_columnwriter(DataType type, DataTypeNullable nullable) {
+    ColumnWriterFn columnwriter;
+
+    if (nullable == DataTypeNullable::Nullable) {
+        visit(type, [&](auto traits) {
+            columnwriter = [](ValueVariant *value, clickhouse::Column &column) {
+                using ColumnType = clickhouse::ColumnNullableT<typename decltype(traits)::ColumnType>;
+                using ValueType = std::invoke_result_t<decltype(decltype(traits)::Getter), fds_drec_field>;
+                auto *col = dynamic_cast<ColumnType*>(&column);
+                if (!value) {
+                    col->Append(std::nullopt);
+                } else {
+                    col->Append(std::get<ValueType>(*value));
+                }
+            };
+        });
+
+    } else {
+        visit(type, [&](auto traits) {
+            columnwriter = [](ValueVariant *value, clickhouse::Column &column) {
+                using ColumnType = typename decltype(traits)::ColumnType;
+                using ValueType = std::invoke_result_t<decltype(decltype(traits)::Getter), fds_drec_field>;
+                static const auto ZeroValue = ValueType{};
+                auto *col = dynamic_cast<ColumnType*>(&column);
+                if (!value) {
+                    col->Append(ZeroValue);
+                } else {
+                    col->Append(std::get<ValueType>(*value));
+                }
+            };
+        });
+    }
+
+    return columnwriter;
+}
+
+std::string type_to_clickhouse(DataType type, DataTypeNullable nullable) {
+    std::string result;
+    visit(type, [&](auto traits) {
+        result = traits.ClickhouseTypeName;
+        if (nullable == DataTypeNullable::Nullable) {
+            result = "Nullable(" + result + ")";
+        }
+    });
+    return result;
+}
---
+++ extra_plugins/output/clickhouse/src/datatype.h
@@ -0,0 +1,216 @@
+/**
+ * @file
+ * @author Michal Sedlak <sedlakm@cesnet.cz>
+ * @brief Functions specific to individual data types
+ * @date 2024
+ *
+ * Copyright(c) 2024 CESNET z.s.p.o.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#pragma once
+
+#include "clickhouse.h"
+
+#include <fmt/core.h>
+#include <libfds.h>
+
+#include <functional>
+#include <string>
+#include <variant>
+
+
+/** An intermediary data type used for conversions between IPFIX and Clickhouse types */
+enum class DataType {
+    Invalid,
+
+    // Lowest to highest precision. Keep it that way!
+    // This is used to simplify upcasting in the unify_type function.
+    // e.g. std::max(Int8, Int16) = Int16
+    Int8,
+    Int16,
+    Int32,
+    Int64,
+
+    UInt8,
+    UInt16,
+    UInt32,
+    UInt64,
+
+    IP, // IP address regardless of version
+    IPv4,
+    IPv6,
+
+    String,
+
+    DatetimeSecs,
+    DatetimeMillisecs,
+    DatetimeMicrosecs,
+    DatetimeNanosecs,
+};
+
+enum class DataTypeNullable {
+    Nonnullable,
+    Nullable,
+};
+
+/**
+ * @brief Simple in_addr wrapper
+ */
+struct IP4Addr : public in_addr {
+    IP4Addr() {
+        s_addr = 0;
+    }
+
+    IP4Addr(in_addr addr) : in_addr(addr) {}
+
+    friend bool operator==(const IP4Addr &a, const IP4Addr &b) {
+        return a.s_addr == b.s_addr;
+    }
+
+    friend bool operator!=(const IP4Addr &a, const IP4Addr &b) {
+        return !(a == b);
+    }
+};
+
+/**
+ * @brief Simple in6_addr wrapper
+ */
+struct IP6Addr : public in6_addr {
+    IP6Addr() {
+        std::memset(s6_addr, 0, sizeof(s6_addr));
+    }
+
+    IP6Addr(in6_addr addr) : in6_addr(addr) {}
+
+    friend bool operator==(const IP6Addr &a, const IP6Addr &b) {
+        return std::memcmp(&a.s6_addr, &b.s6_addr, sizeof(a.s6_addr)) == 0;
+    }
+
+    friend bool operator!=(const IP6Addr &a, const IP6Addr &b) {
+        return !(a == b);
+    }
+};
+
+
+/** Formatter for the DataType enum */
+template <> struct fmt::formatter<DataType> : formatter<string_view> {
+    auto format(DataType type, format_context& ctx) const -> format_context::iterator {
+        const std::string_view name = [=]() {
+            switch (type) {
+            case DataType::Invalid: return "Invalid";
+            case DataType::Int8: return "Int8";
+            case DataType::Int16: return "Int16";
+            case DataType::Int32: return "Int32";
+            case DataType::Int64: return "Int64";
+            case DataType::UInt8: return "UInt8";
+            case DataType::UInt16: return "UInt16";
+            case DataType::UInt32: return "UInt32";
+            case DataType::UInt64: return "UInt64";
+            case DataType::IP: return "IP";
+            case DataType::IPv4: return "IPv4";
+            case DataType::IPv6: return "IPv6";
+            case DataType::String: return "String";
+            case DataType::DatetimeNanosecs: return "DatetimeNanosecs";
+            case DataType::DatetimeMicrosecs: return "DatetimeMicrosecs";
+            case DataType::DatetimeMillisecs: return "DatetimeMillisecs";
+            case DataType::DatetimeSecs: return "DatetimeSecs";
+            }
+            return "???";
+        }();
+        return formatter<string_view>::format(name, ctx);
+    }
+};
+
+/** Formatter for the iemgr element type */
+template <> struct fmt::formatter<fds_iemgr_element_type> : formatter<string_view> {
+    auto format(fds_iemgr_element_type type, format_context& ctx) const -> format_context::iterator {
+        const std::string_view name = [=]() {
+            switch (type) {
+            case FDS_ET_OCTET_ARRAY: return "OCTET_ARRAY";
+            case FDS_ET_UNSIGNED_8: return "UNSIGNED_8";
+            case FDS_ET_UNSIGNED_16: return "UNSIGNED_16";
+            case FDS_ET_UNSIGNED_32: return "UNSIGNED_32";
+            case FDS_ET_UNSIGNED_64: return "UNSIGNED_64";
+            case FDS_ET_SIGNED_8: return "SIGNED_8";
+            case FDS_ET_SIGNED_16: return "SIGNED_16";
+            case FDS_ET_SIGNED_32: return "SIGNED_32";
+            case FDS_ET_SIGNED_64: return "SIGNED_64";
+            case FDS_ET_FLOAT_32: return "FLOAT_32";
+            case FDS_ET_FLOAT_64: return "FLOAT_64";
+            case FDS_ET_BOOLEAN: return "BOOLEAN";
+            case FDS_ET_MAC_ADDRESS: return "MAC_ADDRESS";
+            case FDS_ET_STRING: return "STRING";
+            case FDS_ET_DATE_TIME_SECONDS: return "DATE_TIME_SECONDS";
+            case FDS_ET_DATE_TIME_MILLISECONDS: return "DATE_TIME_MILLISECONDS";
+            case FDS_ET_DATE_TIME_MICROSECONDS: return "DATE_TIME_MICROSECONDS";
+            case FDS_ET_DATE_TIME_NANOSECONDS: return "DATE_TIME_NANOSECONDS";
+            case FDS_ET_IPV4_ADDRESS: return "IPV4_ADDRESS";
+            case FDS_ET_IPV6_ADDRESS: return "IPV6_ADDRESS";
+            case FDS_ET_BASIC_LIST: return "BASIC_LIST";
+            case FDS_ET_SUB_TEMPLATE_LIST: return "SUB_TEMPLATE_LIST";
+            case FDS_ET_SUB_TEMPLATE_MULTILIST: return "SUB_TEMPLATE_MULTILIST";
+            case FDS_ET_UNASSIGNED: return "UNASSIGNED";
+            default: return "???";
+            }
+        }();
+        return formatter<string_view>::format(name, ctx);
+    }
+};
+
+/**
+ * @brief Get intermediary data type for a corresponding IPFIX element type
+ *
+ * @param type The IPFIX type
+ * @return The intermediary data type
+ */
+DataType type_from_ipfix(fds_iemgr_element_type type);
+
+/**
+ * @brief Get Clickhouse data type for the intermediary data type
+ *
+ * @param type The intermediary data type
+ * @param nullable Whether the type is nullable or not
+ * @return The Clickhouse data type
+ */
+std::string type_to_clickhouse(DataType type, DataTypeNullable nullable);
+
+/**
+ * @brief Find an intermediary data type that can be used to store all the possible data types of the alias
+ *
+ * @param alias  The iemgr alias
+ * @return The common data type
+ */
+DataType find_common_type(const fds_iemgr_alias &alias);
+
+
+using ValueVariant = std::variant<
+    std::monostate,
+    uint8_t,
+    uint16_t,
+    uint32_t,
+    uint64_t,
+    int8_t,
+    int16_t,
+    int32_t,
+    int64_t,
+    IP4Addr,
+    IP6Addr,
+    std::string
+>;
+
+using GetterFn = std::function<void (fds_drec_field field, ValueVariant &value)>;
+using ColumnWriterFn = std::function<void (ValueVariant *value, clickhouse::Column &column)>;
+
+GetterFn make_getter(DataType type);
+
+ColumnWriterFn make_columnwriter(DataType type, DataTypeNullable nullable);
+
+/**
+ * @brief Make a Clickhouse column that is able to store values of the supplied data type
+ *
+ * @param type The data type
+ * @param nullable Whether the type is nullable or not
+ * @return The Clickhouse column object
+ */
+std::shared_ptr<clickhouse::Column> make_column(DataType type, DataTypeNullable nullable);
