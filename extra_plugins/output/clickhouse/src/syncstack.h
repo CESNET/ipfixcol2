6e17fb6f35ff6d599c65838feb5bd379a326dedd
Michal Sedlak <sedlakm@cesnet.cz>
2025-01-09 13:46:16 +0100

    Clickhouse - introduce SyncStack class

---
+++ extra_plugins/output/clickhouse/src/syncstack.h
@@ -0,0 +1,70 @@
+/**
+ * @file
+ * @author Michal Sedlak <sedlakm@cesnet.cz>
+ * @brief SyncStack class implementation
+ * @date 2024
+ *
+ * Copyright(c) 2024 CESNET z.s.p.o.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#pragma once
+
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <stack>
+
+/**
+ * @brief A thread-safe stack
+ */
+template <typename Item>
+class SyncStack {
+public:
+    /**
+     * @brief Put an item into the queue
+     *
+     * @param item The item
+     */
+    void put(Item item)
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_items.push(item);
+        m_size = m_items.size();
+        m_avail_cv.notify_all();
+    }
+
+    /**
+     * @brief Get an item from the queue, block and wait if there aren't any
+     *
+     * @return The item
+     */
+    Item get()
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        while (true) {
+            if (!m_items.empty()) {
+                auto item = m_items.top();
+                m_items.pop();
+                m_size = m_items.size();
+                return item;
+            }
+            m_avail_cv.wait(lock);
+        }
+    }
+
+    /**
+     * @brief Get the current size of the queue
+     *
+     * @return The number of items in the queue
+     */
+    std::size_t size()
+    {
+        return m_size;
+    }
+
+private:
+    std::atomic_size_t m_size = 0;
+    std::stack<Item> m_items;
+    std::mutex m_mutex;
+    std::condition_variable m_avail_cv;
+};
